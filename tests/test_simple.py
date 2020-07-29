2563701e57f43d8709465d737b85a1069bb00a14
Prabal Deb <prdeb@microsoft.com>
2020-07-29 09:48:20 +0530

    Adding failed test case

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,6 +1,6 @@
 # the inclusion of the tests module is not meant to offer best practices for
 # testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
+# setup.py that excludes installing the "tests" package 1
 
 import unittest
 
